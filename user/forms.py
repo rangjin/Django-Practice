e579b6b1fa41e3cbaa599529a2cef0facf34f83b
rangjin <46679581+rangjin@users.noreply.github.com>
2020-09-03 07:59:11 +0900

    로그인 생성

--- user/forms.py
+++ user/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.hashers import make_password, check_password
 from .models import User
 
 
@@ -37,7 +38,39 @@ class RegisterForm(forms.Form):
             else:
                 user = User(
                     email=email, 
-                    password=password
+                    password=make_password(password)
                 )
 
                 user.save()
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(
+        error_messages={
+            'required': '이메일을 입력해주세요.'
+        }, 
+        max_length=64, label='이메일', 
+    )
+
+    password = forms.CharField(
+        error_messages={
+            'required': '비밀번호를 입력해주세요.'
+        }, 
+        widget=forms.PasswordInput, label='비밀번호', 
+    )
+
+    def clean(self):
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        password = cleaned_data.get('password')
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            self.add_error('email', '아이디가 없습니다.')
+            return
+
+        if not check_password(password, user.password):
+            self.add_error('password', '비밀번호가 일치하지 않습니다.')
+        else:
+            self.email = email
