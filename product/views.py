d1cf3e0ae30c0f3de39e5a124708fe43c366e9b7
rangjin <46679581+rangjin@users.noreply.github.com>
2020-09-03 17:54:24 +0900

    상품 주문 생성

--- product/views.py
+++ product/views.py
@@ -3,6 +3,8 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView
 from .models import Product
 from .forms import RegisterForm
+from order.forms import RegisterForm as OrderForm
+
 
 # Create your views here.
 
@@ -12,12 +14,20 @@ class ProductList(ListView):
     template_name = 'product.html'
     context_object_name = 'product_list'
 
+
 class ProductCreate(FormView):
     template_name = 'register_product.html'
     form_class = RegisterForm
     success_url = '/product/'
 
+
 class ProductDetail(DetailView):
     template_name = 'product_detail.html'
     queryset = Product.objects.all()
     context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = OrderForm(self.request)
+
+        return context
