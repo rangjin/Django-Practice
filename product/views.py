499a2553fd0c534e2e4d00c25e6e7b9e6f4bc5b2
rangjin <46679581+rangjin@users.noreply.github.com>
2020-09-05 15:30:04 +0900

    api 제작

--- product/views.py
+++ product/views.py
@@ -1,14 +1,34 @@
-from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView
+from rest_framework import generics, mixins
 from .models import Product
 from .forms import RegisterForm
+from .serializers import ProductSerializer
 from order.forms import RegisterForm as OrderForm
 from user.decorators import admin_required
 
-
 # Create your views here.
+
+
+class ProductListAPI(generics.GenericAPIView, mixins.ListModelMixin):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all().order_by('id')
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class ProductDetailAPI(generics.GenericAPIView, mixins.RetrieveModelMixin):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all().order_by('id')
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
 
 class ProductList(ListView):
